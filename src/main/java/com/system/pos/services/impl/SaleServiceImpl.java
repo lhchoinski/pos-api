3b6b34e2694205e20395f612ce6075b59dfbbee1
Luiz Henrique <lhchoinski04@gmail.com>
2025-01-20 19:16:25 -0300

    fix

--- src/main/java/com/system/pos/services/impl/SaleServiceImpl.java
+++ src/main/java/com/system/pos/services/impl/SaleServiceImpl.java
@@ -3,8 +3,6 @@ package com.system.pos.services.impl;
 import com.system.pos.dtos.SaleDTO;
 import com.system.pos.dtos.SaleItemDTO;
 import com.system.pos.entities.Customer;
-import com.system.pos.entities.Sale;
-import com.system.pos.entities.SaleItem;
 import com.system.pos.exeptions.NotFoundException;
 import com.system.pos.mappers.SaleMapper;
 import com.system.pos.producers.SaleProducer;
@@ -15,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,8 +24,6 @@ public class SaleServiceImpl implements SaleService {
     private final SaleProducer saleProducer;
 
     public SaleDTO processSale(SaleDTO saleDTO) {
-
-        UUID uuid = UUID.fromString("69da7132-2d43-4353-8de7-757f4f0f5a69");
 
         Customer customer = getCustomer(saleDTO.getCustomerId());
 
